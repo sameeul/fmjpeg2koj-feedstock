bd3e07c91ef6d72e1aeaf2d0637e17b079d98a7e
Sameeul B Samee <sameeul@gmail.com>
2025-08-05 12:44:58 -0400

    update windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,10 @@
 mkdir build
 cd build
 
-cmake ^
-  -DCMAKE_CXX_STANDARD=11 ^
-  -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
-  -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG /std:c++11" ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  -DFMJPEG2K=%LIBRARY_PREFIX% ^
-  -DBUILD_SHARED_LIBS=ON ^
-  ..
-if errorlevel 1 exit 1
 
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DFMJPEG2K=%LIBRARY_PREFIX% ..
+
+if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install --parallel
 if errorlevel 1 exit 1
